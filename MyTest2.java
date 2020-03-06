8d2e768044ab8c0a957b61e6f36d9981fbc72794
Amrit <amrit.svnit@gmail.com>
2020-03-07 00:22:07 +0530

    My commit from 2nd branch

--- MyTest2.java
+++ MyTest2.java
@@ -1,5 +1,5 @@
 class MyTest2{
 	public static void main(String args[]){
-		System.out.println("My Second git program but I have changed something here");	
+		System.out.println("My Second git program but I have changed something here from 2nd branch");	
 	}
 }
